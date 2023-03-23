576c8d9f61c5a6b0f06e2805e521db20cd411f7f
张婉倩 <zhangwanqian@zhangwanqiandeMacBook-Air.local>
2023-03-24 00:07:42 +0800

    update

--- app.py
+++ app.py
@@ -1,108 +1,16 @@
-# import cv2
-# import os
-# from flask import Flask, render_template, Response, request,redirect, url_for
-
-# app = Flask(__name__)
-
-# cap = cv2.VideoCapture(0)
-# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-
-# def detect_color(frame):
-#     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-#     height, width, _ = frame.shape
-
-#     cx = int(width / 2)
-#     cy = int(height / 2)
-
-#     # Pick pixel value
-#     pixel_center = hsv_frame[cy, cx]
-#     hue_value = pixel_center[0]
-
-#     color = "Undefined"
-#     if hue_value < 5:
-#         color = "RED"
-#     elif hue_value < 22:
-#         color = "ORANGE"
-#     elif hue_value < 33:
-#         color = "YELLOW"
-#     elif hue_value < 78:
-#         color = "GREEN"
-#     elif hue_value < 131:
-#         color = "BLUE"
-#     elif hue_value < 170:
-#         color = "VIOLET"
-#     else:
-#         color = "RED"
-
-#     pixel_center_bgr = frame[cy, cx]
-#     b, g, r = int(pixel_center_bgr[0]), int(pixel_center_bgr[1]), int(pixel_center_bgr[2])
-
-#     cv2.rectangle(frame, (cx - 220, 10), (cx + 200, 120), (255, 255, 255), -1)
-#     cv2.putText(frame, color, (cx - 200, 100), 0, 3, (b, g, r), 5)
-#     cv2.circle(frame, (cx, cy), 5, (25, 25, 25), 3)
-
-#     ret, jpeg = cv2.imencode('.jpg', frame)
-#     return jpeg.tobytes(), color
-
-# def gen():
-#     while True:
-#         success, frame = cap.read()
-#         if not success:
-#             break
-
-#         frame_bytes, color = detect_color(frame)
-
-#         yield (b'--frame\r\n'
-#                b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
-
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
-# @app.route('/video_feed')
-# def video_feed():
-#     return Response(gen(),
-#                     mimetype='multipart/x-mixed-replace; boundary=frame')
-
-# @app.route('/', methods=['POST'])
-# def detect():
-#     success, frame = cap.read()
-#     if not success:
-#         return "Error: Failed to capture frame from camera"
-    
-#     # Detect the color of the object in the frame
-#     color = detect_color(frame)
-#     filename = os.path.join(app.root_path, 'img', color[1]+'.jpg')
-#     cv2.imwrite(filename, frame)
-#     # Return the color as a string
-#     # print(color[0])
-#     # cap.release()
-    
-#     return color
-    
-# @app.route('/home')
-# def home():
-#     return "Hello, World!"
-
-# if __name__ == '__main__':
-#     app.run()
-
-
-from flask import Flask, render_template, Response, redirect, url_for
 import cv2
-import os
+import numpy as np
+from flask import Flask, render_template, Response, request
 
 app = Flask(__name__)
 
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
-# Flag variable to keep track of whether the camera preview is running or not
-camera_running = True
-camera_capture = None
+@app.route('/')
+def index():
+    """Render the index page."""
+    return render_template('index.html')
+
+
 def detect_color(frame):
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     height, width, _ = frame.shape
@@ -130,86 +38,44 @@ def detect_color(frame):
     else:
         color = "RED"
 
-    pixel_center_bgr = frame[cy, cx]
-    b, g, r = int(pixel_center_bgr[0]), int(pixel_center_bgr[1]), int(pixel_center_bgr[2])
+    return color
 
-    cv2.rectangle(frame, (cx - 220, 10), (cx + 200, 120), (255, 255, 255), -1)
-    cv2.putText(frame, color, (cx - 200, 100), 0, 3, (b, g, r), 5)
-    cv2.circle(frame, (cx, cy), 5, (25, 25, 25), 3)
 
-    ret, jpeg = cv2.imencode('.jpg', frame)
-    return jpeg.tobytes(), color
-
-def gen():
+def gen_frames():
+    """Generate frames from the user's camera."""
+    cap = cv2.VideoCapture(0)
     while True:
         success, frame = cap.read()
         if not success:
             break
-
-        frame_bytes, color = detect_color(frame)
-
+        color = detect_color(frame)
+        if color is not None:
+            (x, y) = (color[0], color[1])
+            cv2.putText(
+                frame, f"Rainbow color: {color}", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            cv2.rectangle(frame, (x - 10, y - 10),
+                          (x + 10, y + 10), (0, 0, 255), 2)
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-
-def gen():
-    while True:
-        if camera_running:
-            success, frame = cap.read()
-            if not success:
-                break
-
-            frame_bytes, color = detect_color(frame)
-
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
-
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    """Stream the camera preview with rainbow color detection."""
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route('/', methods=['POST'])
-def detect():
-    # global camera_running  # Use the global keyword to modify the flag variable
-    # if camera_running is False:
-        # return "Please click the continue"
-    success, frame = cap.read()
-    if not success:
-        return "Error: Failed to capture frame from camera"
-    
-    # Detect the color of the object in the frame
-    color = detect_color(frame)
-    filename = os.path.join(app.root_path, 'img', color[1]+'.jpg')
-    cv2.imwrite(filename, frame)
 
-    # Release the VideoCapture object to stop the camera preview
-    # cap.release()
-    # camera_running = False  # Set the sflag variable to False to stop the camera preview
-    
-    # Return the color as a string
-    return color
+@app.route('/detect_color', methods=['POST'])
+def detect_color_api():
+    """Detect the rainbow color in a given image."""
+    file = request.files['image']
+    img = cv2.imdecode(np.fromstring(file.read(), np.uint8), cv2.IMREAD_COLOR)
+    color = detect_color(img)
+    if color is not None:
+        return f"Rainbow color: {color}", 200
+    else:
+        return "Rainbow color not found", 404
 
-# @app.route('/continue')
-# def continue_preview():
-#     global cap, camera_running  # Use the global keyword to modify the flag variables
-
-#     if not camera_running:
-#         # Create a new VideoCapture object to start the camera preview again
-#         # if cap is not None:
-#         #     cap.release()
-#         cap = cv2.VideoCapture(0)
-#         cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-#         cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-#         camera_running = True  # Set the flag variable to True to resume the camera preview
-
-#     return render_template('index.html')
-
-# if __name__ == '__main__':
-
-#     app.run()
 
